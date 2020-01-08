9d68352e1a8d785ab25c795c99c35f593f8772f2
Matheus Rodrigues <matheusrod92@gmail.com>
2020-01-08 19:28:50 -0300

    added loadAsync file to react-router extensions

---
+++ src/extensions/react-router/loadAsync.js
@@ -0,0 +1,22 @@
+export const errorLoading = (errorHandler) => (err) => {
+  console.error('Dynamic page loading failed', err); // eslint-disable-line no-console
+  if (
+    process.env.NODE_ENV === 'production' &&
+    typeof errorHandler === 'function'
+  ) {
+    errorHandler(err);
+  }
+};
+
+export const loadModule = (cb) => (componentModule) => {
+  cb(null, componentModule.default);
+};
+
+export default (errorHandler) => (importedComponent, cb) => {
+  const renderRoute = loadModule(cb);
+  Promise.all([importedComponent])
+    .then(([component]) => {
+      renderRoute(component);
+    })
+    .catch(errorLoading(errorHandler));
+};
---
+++ src/extensions/react-router/tests/loadAsync.test.js
@@ -0,0 +1,7 @@
+import loadAsync, { loadModule, errorLoading } from '../loadAsync';
+
+describe('loadAsync', () => {
+  it('should return true', () => {
+    expect(true).toBeTruthy();
+  });
+});
