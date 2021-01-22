da9610a0b32f64f032831058c7cf106427576eb6
Raphael Brandão <raphael.brandao@quintoandar.com.br>
2021-01-22 15:14:08 -0300

    replace redux-immutable with redux-immer

--- src/extensions/redux/reducers.js
+++ src/extensions/redux/reducers.js
@@ -1,4 +1,5 @@
-import { combineReducers } from 'redux-immutable';
+import produce from 'immer';
+import { combineReducers } from 'redux-immer';
 
 export const RESET = 'core/RESET';
 
@@ -6,7 +7,7 @@ export const RESET = 'core/RESET';
  * Creates the main reducer with the asynchronously loaded ones
  */
 export default function createReducer(asyncReducers) {
-  const appReducer = combineReducers({ ...asyncReducers });
+  const appReducer = combineReducers(produce, { ...asyncReducers });
   const reducer = (state, action) => appReducer(action.type === RESET ? undefined : state, action);
   return reducer;
 }
